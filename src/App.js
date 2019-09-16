3d077fb4812d0b009bc4e9199dc6f1a4d1dfe629
Cynnah <massot.dorian@gmail.com>
2019-09-16 19:56:10 +0200

    creation on a componnent with state and display on app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import Lamp from "./Lamp";
 import Quotes from "./Quotes";
 
 class App extends Component {
@@ -12,6 +13,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Simpsons Quotes</h1>
         </header>
+        <Lamp on />
+        <Lamp />
         <Quotes />
       </div>
     );
