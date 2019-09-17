83b1cff552e3ae439fd76bd47bde7ec3cecef3c6
Cynnah <massot.dorian@gmail.com>
2019-09-17 06:11:13 +0200

    creation on a modification on logo when we click on the working button

--- src/App.js
+++ src/App.js
@@ -6,15 +6,36 @@ import Lamp from "./Lamp";
 import Quotes from "./Quotes";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        isWorking: props.isWorking,
+        header: true
+      };
+  }
+
+  handleClickWorking = () => {
+    this.setState({ 
+      isWorking: !this.state.isWorking
+    });
+  };
+
   render() {
+    const isWorking = this.state.isWorking ? 'isWorking' : 'isNotWorking';
+    const logotourne = this.state.on ? "App-logo" : "App-logo2";
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header className="header">
+          {this.state.isWorking ? <img src={logo} className={logotourne} alt="logo" /> : <img src={logo} className="App-logo" alt="logo" />}
           <h1 className="App-title">Simpsons Quotes</h1>
         </header>
         <Lamp on />
         <Lamp />
+        <button 
+            className={isWorking} 
+            onClick={this.handleClickWorking}
+        >{isWorking}</button>
         <Quotes />
       </div>
     );
